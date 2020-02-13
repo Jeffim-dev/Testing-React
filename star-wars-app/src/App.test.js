56e9dacbc413381664dd047311f4035c9dbb058d
unknown <everest.k6@gmail.com>
2020-02-13 09:27:07 -0800

    second commit

--- star-wars-app/src/App.test.js
+++ star-wars-app/src/App.test.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
-import App from './App';
 import StarWarsCharacters from './components/StarWarsCharacters';
 import { getData as mockGetData} from './api';
 
-// test("Render the heading", async () => {  
-//   const wrapper = rtl.render(<App />);   
-//   await wrapper.findAllByAltText(/logo/i);
-// });
+jest.mock('./api')
+
+test(' Made API call', async () => {
+  mockGetData.mockResolvedValueOnce({
+    results: 
+     [{
+       name: "Luke Skywalker",
+       height: "172",
+       mass: "77", 
+     }]
+  })
+  
+  const wrapper = rtl.render(<StarWarsCharacters />); 
+  await wrapper.findByText(/luke/i);
+    
+  expect(mockGetData).toHaveBeenCalledTimes(1);
+
+
+  await wrapper.getByText(/next/i);
+  const nextButton = wrapper.getByText(/next/i)
+   
+    rtl.act(() => {
+      rtl.fireEvent.click(nextButton)
+    })
+
+
+  await wrapper.getByText(/previous/i);
+  const prevButton = wrapper.getByText(/previous/i)
+     
+    rtl.act(() => {
+      rtl.fireEvent.click(prevButton)
+    })
+}) 
 
 // test("Previous Button", async () => {
 //   const wrapper = rtl.render(<StarWarsCharacters />); 
@@ -30,21 +58,3 @@ import { getData as mockGetData} from './api';
 //     rtl.fireEvent.click(nextButton)
 //   })
 // })
-
-jest.mock('./api')
-
-test(' Made API call', async () => {
-  mockGetData.mockResolvedValueOnce({results: 
-    [{
-      name: "Luke Skywalker",
-      height: "172",
-      mass: "77", 
-
-    }]
-  })
-  
-  const wrapper = rtl.render(<StarWarsCharacters />); 
-  await wrapper.findByText(/luke/i);
-    
-  expect(mockGetData).toHaveBeenCalled();
-}) 
